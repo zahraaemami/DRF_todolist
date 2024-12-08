0592c03bcee4ceafcf5ea6862d1ba5378386fe05
zahra <zahraaeemami@gmail.com>
2024-12-08 19:34:41 +0330

    add serializer , view

---
+++ authentication/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer) :
+    
+    password = serializers.CharField(max_length = 150 , write_only =  True)
+    
+    
+    class Meta :
+        
+        model = User 
+        fields = ['username', 'email','password']
+        
+        
+        def create (self , validated_data) :
+            
+            return User.objects.create_user (**validated_data)
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework .response import Response
+from rest_framework.generics import GenericAPIView
+from rest_framework import status
 
-# Create your views here.
+from .serializers import UserSerializer
+from .models import User
+
+
+
+class RegisterView(GenericAPIView) :
+    
+    serializer_class = UserSerializer
+    
+    def post (self , request) :
+        serializer = UserSerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response (serializer.data , status = 200)
