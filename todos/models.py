69fd31d69bff2db4643c6e3e2212bf136b2b2f06
zahra <zahraaeemami@gmail.com>
2024-12-09 21:17:15 +0330

    add todo model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from helpers.models import BaseModel
+from authentication.models import User 
 
-# Create your models here.
+
+class Todo (BaseModel) :
+    
+    title = models.CharField( max_length=50 ,blank = True , default = '')
+    description = models.TextField()
+    is_complete = models.BooleanField(default = False)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    
+    def __str__(self) :
+        return self.title
